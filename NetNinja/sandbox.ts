4081e1a912755a48d8c2a8a7724cb6ff803e85ce
Sunjay77 <sanjaylamaa77@gmail.com>
2026-05-18 13:39:19 +0545

    arrays and objects

--- NetNinja/sandbox.ts
+++ NetNinja/sandbox.ts
@@ -25,3 +25,43 @@ const circ = (diameter: number) => {
 };
 
 console.log(circ(8)); 
+
+//Arrays
+
+let names = ['sunjay', 'jay', 'david'];
+
+names.push('waiwai');
+// names.push(1) only strings can be added
+
+
+let numbers = [1, 2, 3];
+numbers.push(23); // works with numbers when it is number above
+// numbers.push('wau') shows error
+// numbers[1]='same' same thing
+
+let mixed = ['sunjay', 25, true];
+
+mixed.push('waiwai'); // works with any type in the array
+mixed.push(30);
+mixed.push(false);
+mixed[0] = 3;
+
+//objects
+let sun = {
+  name: 'jay',
+  age: 25,
+  belt: 'black'
+};
+
+sun.age = 30;
+sun.name = 'waiwai';
+// sun.age = 'sa'; doesnot takes string
+// sun.skills = ['javascript', 'typescript'];
+
+sun = {
+  name: "waiwai",
+  belt: 'purple',
+  age: 30 // cannot miss any properties because doesnot match the initial structure should be same structure
+  // height: 5.9; can't add other properties too
+}
+// TS is strict
