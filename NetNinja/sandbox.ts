96182f62016a70caa06f366e82d37a731100b541
Sunjay77 <sanjaylamaa77@gmail.com>
2026-05-18 09:57:21 +0545

    Basics of types

--- NetNinja/sandbox.ts
+++ NetNinja/sandbox.ts
@@ -8,3 +8,20 @@ console.log(inputs);
 inputs.forEach(input => {
   console.log(input);
 });
+
+//Type Basics
+let character2 = "david";
+let age = 25;
+let isBlackBelt = false;
+
+// character2 = 30; you cant change it on number
+// character2 = "waiwai" you can since it is on string
+
+// isBlackBelt = "yes" error
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(8)); 
